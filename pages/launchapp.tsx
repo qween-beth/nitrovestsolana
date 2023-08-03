72ad24e5dd7fdd0575c942e9e676c724153c5d13
Elizabeth O. Edwards <103456558+qween-beth@users.noreply.github.com>
2023-08-03 22:30:52 +0100

    Update launchapp.tsx

--- pages/launchapp.tsx
+++ pages/launchapp.tsx
@@ -1,4 +1,6 @@
 import React, { useState, ChangeEvent, FormEvent } from "react";
+import type { NextPage } from "next";
+import NavbarContainer from "../components/navbar-container";
 import { ThirdwebSDK } from "@thirdweb-dev/sdk/solana";
 import { privateKey } from "../privateKey.mjs";
 import { readFileSync } from "fs";
@@ -10,7 +12,7 @@ const MyNFTPage: React.FC = () => {
     name: "",
     description: "",
     image: null as File | null,
-    imageUrl: "",
+    imageUrl: ""
   });
   const [mintedNfts, setMintedNfts] = useState<string[]>([]);
 
@@ -28,7 +30,7 @@ const MyNFTPage: React.FC = () => {
           const metadata = {
             name: nftMetadata.name,
             description: nftMetadata.description,
-            image: event.target.result as string,
+            image: event.target.result as string
           };
 
           const myNftCollection = await sdk.getNFTCollection(
@@ -56,64 +58,83 @@ const MyNFTPage: React.FC = () => {
       setNftMetadata((prevMetadata) => ({
         ...prevMetadata,
         image: files[0],
-        imageUrl,
+        imageUrl
       }));
     } else {
       setNftMetadata((prevMetadata) => ({
         ...prevMetadata,
-        [name]: value,
+        [name]: value
       }));
     }
   };
 
   return (
-    <div>
-      <h1>Mint NFT</h1>
-      <form onSubmit={handleFormSubmit}>
-        <label>
-          NFT Name:
-          <input
-            type="text"
-            name="name"
-            value={nftMetadata.name}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          NFT Description:
-          <input
-            type="text"
-            name="description"
-            value={nftMetadata.description}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          NFT Image:
-          <input type="file" name="image" onChange={handleInputChange} />
-        </label>
-        <br />
-        <button type="submit">Mint NFT</button>
-      </form>
+  <div>
+    <NavbarContainer />
+   
+<div className="w-11/12 mx-auto my-2 flex flex-row items-center justify-center gap-3">
 
-      <h2>Minted NFTs</h2>
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <h1 className="text-2xl font-bold mb-4">Mint NFT</h1>
+      <form onSubmit={handleFormSubmit} className="mb-4">
+  <label className="block mb-2 text-white">
+    NFT Name:
+    <input
+      type="text"
+      name="name"
+      value={nftMetadata.name}
+      onChange={handleInputChange}
+      className="border border-gray-300 px-3 py-2 mt-1 rounded-md w-64"
+    />
+  </label>
+  <br />
+  <label className="block mb-2 text-white">
+    NFT Description:
+    <input
+      type="text"
+      name="description"
+      value={nftMetadata.description}
+      onChange={handleInputChange}
+      className="border border-gray-300 p-2 mt-4"
+    />
+  </label>
+  <br />
+  <label className="block mb-2 text-white ">
+    NFT Image:
+    <input
+      type="file"
+      name="image"
+      onChange={handleInputChange}
+      className="mt-1 w-[74%] h-12 text-white bg-mediumblue-100 font-semibold flex flex-col items-center justify-center"
+    />
+  </label>
+  <br></br>
+  
+  <button type="submit" className="w-[45%] h-12 text-white bg-mediumblue-100 font-semibold flex flex-col items-center justify-center">
+          Mint NFT
+        </button>
+      
+
+</form>
+
+
+<br/>
+<br/>
+
+      <h2 className="text-xl font-bold mb-4  text-white">Minted NFTs</h2>
+      <div className="flex flex-wrap">
         {mintedNfts.map((nft) => (
           <div
             key={nft}
-            style={{
-              border: "1px solid #000",
-              padding: "1rem",
-              margin: "1rem",
-            }}
+            className="border border-gray-300 p-4 m-4"
+            style={{ maxWidth: "200px" }}
           >
-            <img src={nft} alt="Minted NFT" style={{ width: "200px" }} />
+            <img src={nft} alt="Minted NFT" className="w-full" />
           </div>
         ))}
       </div>
     </div>
+</div>
+    
   );
 };
 
